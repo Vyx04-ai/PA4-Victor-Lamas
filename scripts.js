d73413ef410e8ed438f49264ab4da61c07fbfdd4
Victor Lamas <victorjoselamaschacon@gmail.com>
2025-11-26 22:54:47 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode: 'fade',
+        auto: true,
+        pause: 3000,
+        controls: false,
+        pager: false,
+        speed: 1000,
+        stopAutoOnClick: true,
+        touchEnabled: false,
+    });
+});
